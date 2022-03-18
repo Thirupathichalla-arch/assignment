95fcb43dd813901d8872ef3742bd57210d76e1c4
Thirupathichalla-arch <91534651+Thirupathichalla-arch@users.noreply.github.com>
2022-03-18 13:27:39 +0530

    partiallcommit

--- instaminutes/index.js
+++ instaminutes/index.js
@@ -1,34 +1,13 @@
 const schedule = require('node-schedule');
-
-let data = [
-    { 
-        "text": "textOne", 
-        "dateTime": "2020,07,15,15,00,00"
-    },
-    { 
-        "text": "textTwo", 
-        "dateTime": "2020,07,15,14,00,00"
-    },
-    { 
-        "text": "textThree", 
-        "dateTime": "2020,07,15,15,00,00"
+let i = 0;
+let arr = ["one", "two", "three","four","five","six","seven","eight","nine","ten"];
+const job = schedule.scheduleJob('*/7 * * * * *', function () {
+    if (i == 9) {
+        schedule.cancelJob(job);
     }
-]
-//console.log(data[0].text)
-let rule = new schedule.RecurrenceRule();
-data.forEach((e) => {
-    let d = e.dateTime.split(",");
-        rule.second = d[5],
-        rule.minute = d[4],
-        rule.hour = d[3],
-        rule.date = d[2],
-        rule.month = d[1],
-        rule.year = d[0];
-  
-})
-    //console.log(rule)
-const job = schedule.scheduleJob(rule, function () {
-    data.forEach((e) => {
-         console.log(e.text)
-     })
- })
+    console.log(`"text":text${arr[i]}`);
+    console.log(`"dateTime":`,new Date())
+    i++;
+}) 
+
+
