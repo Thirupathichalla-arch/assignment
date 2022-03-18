5a2c55c1b0e91aeb6739552d96673f471f945378
Thirupathichalla-arch <91534651+Thirupathichalla-arch@users.noreply.github.com>
2022-03-18 18:52:16 +0530

    finalcommit

--- instaminutes/index.js
+++ instaminutes/index.js
@@ -1,62 +1,65 @@
-const schedule = require('node-schedule');
+const cron =require("node-cron");
 let arr = [
     {
-        "text": "textOne",
-        "dateTime": "*/7 * * * * *"
+        text: "textOne",
+        dateTime: new Date("May 14, 2024 16:57:00"),
     },
     {
-        "text": "textTwo",
-        "dateTime": "*/7 * * * * *"
+        text: "textTwo",
+        dateTime: new Date("May 16, 2024 21:53:00"),
     },
     {
-        "text": "textThree",
-        "dateTime": "*/7 * * * * *"
+        text: "textThree",
+        dateTime: new Date("March 18, 2022 18:35:00"),
     },
     {
-        "text": "textFour",
-        "dateTime": "*/7 * * * * *"
+        text: "textFour",
+        dateTime: new Date("March 18, 2022 18:35:09"),
     },
     {
-        "text": "textFive",
-        "dateTime": "*/7 * * * * *"
+        text: "textFive",
+        dateTime: new Date("May 12, 2024 16:53:02"),
     },
     {
-        "text": "textSix",
-        "dateTime": "*/7 * * * * *"
+        text: "textSix",
+        dateTime: new Date("May 14, 2024 16:53:00"),
     },
     {
-        "text": "textSeven",
-        "dateTime": "*/7 * * * * *"
+        text: "textSeven",
+        dateTime: new Date("May 14, 2024 16:53:30"),
     },
     {
-        "text": "textEight",
-        "dateTime": "*/7 * * * * *"
+        text: "textEight",
+        dateTime: new Date("May 14, 2024 16:28:05"),
     },
     {
-        "text": "textNine",
-        "dateTime": "*/7 * * * * *"
+        text: "textNine",
+        dateTime: new Date("May 14, 2024 16:20:02"),
     },
     {
-        "text": "textTen",
-        "dateTime": "*/7 * * * * *"
+        text: "textTen",
+        dateTime: new Date("May 14, 2024 16:00:00"),
     },
 ]
-let i = 0;
-let j = 0;
-let date = arr[j].dateTime;
-const job = schedule.scheduleJob(date, function () {
-     if (i == 9) {
-        schedule.cancelJob(job)
-    }
-    var str = '';
-    let syr=""
-    str += arr[i].text;
-    i++;
-    for (let i = str.length - 1; i >= 0; i--){
-        syr+=str[i]
-    }
-    console.log(syr);
-})
-j++; 
+arr.forEach((e) => {
+//console.log(e.dateTime)
+    let [second, minute, hour, Month, month, Week] = [
+    e.dateTime.getSeconds() + e.text.length,
+    e.dateTime.getMinutes(),
+    e.dateTime.getHours(),
+    e.dateTime.getDate(),
+    e.dateTime.getMonth() + 1,
+    e.dateTime.getDay(),
+    ];
+    //console.log(`${second} ${minute} ${hour} ${dayOfMonth} ${month} ${dayOfWeek}`)
+    cron.schedule(
+        `${second} ${minute} ${hour} ${Month} ${month} ${Week}`,
+        () => {
+            fun(e.text);
+        }
+    )
+});
 
-
+function fun(text) {
+    console.log(text.split("").reverse().join(''));
+}
