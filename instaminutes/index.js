011385665c886ba5f1d7a049e17c52006090a095
Thirupathichalla-arch <91534651+Thirupathichalla-arch@users.noreply.github.com>
2022-03-18 16:14:09 +0530

    prefinal

--- instaminutes/index.js
+++ instaminutes/index.js
@@ -1,13 +1,62 @@
 const schedule = require('node-schedule');
+let arr = [
+    {
+        "text": "textOne",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textTwo",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textThree",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textFour",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textFive",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textSix",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textSeven",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textEight",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textNine",
+        "dateTime": "*/7 * * * * *"
+    },
+    {
+        "text": "textTen",
+        "dateTime": "*/7 * * * * *"
+    },
+]
 let i = 0;
-let arr = ["one", "two", "three","four","five","six","seven","eight","nine","ten"];
-const job = schedule.scheduleJob('*/7 * * * * *', function () {
-    if (i == 9) {
-        schedule.cancelJob(job);
+let j = 0;
+let date = arr[j].dateTime;
+const job = schedule.scheduleJob(date, function () {
+     if (i == 9) {
+        schedule.cancelJob(job)
     }
-    console.log(`"text":text${arr[i]}`);
-    console.log(`"dateTime":`,new Date())
+    var str = '';
+    let syr=""
+    str += arr[i].text;
     i++;
-}) 
+    for (let i = str.length - 1; i >= 0; i--){
+        syr+=str[i]
+    }
+    console.log(syr);
+})
+j++; 
 
 
